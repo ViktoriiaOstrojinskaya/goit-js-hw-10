fedf12d4e280d8bebeb7b889743295a4147e4357
Viktoriia Ostrojinskaya <viktoriia.ostrojinskaya@gmail.com>
2022-10-14 08:31:43 -0400

    part #2

---
+++ src/fetchCountries.js
@@ -0,0 +1,14 @@
+const URL = 'https://restcountries.com';
+
+function fetchCountries(country) {
+  return fetch(
+    `${URL}/v3.1/name/${country}?fields=name,capital,population,flags,languages`
+  ).then(response => {
+    if (!response.ok) {
+      throw new Error('fail');
+    }
+    return response.json();
+  });
+}
+
+export default { fetchCountries };
--- src/index.js
+++ src/index.js
@@ -1,44 +1,26 @@
 import './css/styles.css';
 import debounce from 'lodash.debounce';
 import countryCard from './countryCard.hbs';
+import API from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
-const URL = 'https://restcountries.com';
 
 const input = document.querySelector('#search-box');
 const listCountries = document.querySelector('.country-list');
 
-input.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
+input.addEventListener('input', debounce(onInputCountry, DEBOUNCE_DELAY));
 
-function onInput(event) {
-  const country = input.value;
+function onInputCountry(event) {
+  if (input.value === '') {
+    listCountries.innerHTML = '';
+  }
+  const country = input.value.trim();
   console.log(country);
-  fetchCountries(country).then(createCountryCard);
-}
 
-function fetchCountries(country) {
-  return fetch(
-    `${URL}/v3.1/name/${country}?fields=name,capital,population,flags,languages`
-  ).then(response => {
-    if (!response.ok) {
-      throw new Error('fail');
-    }
-    return response.json();
-  });
+  API.fetchCountries(country).then(createCountryCard);
 }
 
 function createCountryCard(countyInform) {
   const markup = countryCard(countyInform);
   listCountries.innerHTML = markup;
 }
-
-// function createMarkup({ name, capital, population, flags, languages }) {
-//   listCountries.innerHTML = `
-//       <img src=${flags} alt="flag"/>
-//       <h2 class="contry-name">${name}</h2>
-//       <ul class="contry-name__list">
-//       <li class="contry-name__item">Capital:${capital}</li>
-//       <li class="contry-name__item">Population:${population}</li>
-//       <li class="contry-name__item">Languages:${languages}</li>
-//       </ul>`;
-// }
