c9deef4d3e3a25273e514fb322b6aa3226707799
Viktoriia Ostrojinskaya <viktoriia.ostrojinskaya@gmail.com>
2022-10-13 23:42:25 -0400

    part#1

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './css/styles.css';
 import debounce from 'lodash.debounce';
-//import cardCountry from './country-card.hbs';
+import countryCard from './countryCard.hbs';
 
 const DEBOUNCE_DELAY = 300;
 const URL = 'https://restcountries.com';
@@ -11,23 +11,34 @@ const listCountries = document.querySelector('.country-list');
 input.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
 
 function onInput(event) {
-  const name = input.name.value;
-  fetchCountries(name).then(createMarkup);
+  const country = input.value;
+  console.log(country);
+  fetchCountries(country).then(createCountryCard);
 }
 
 function fetchCountries(country) {
-  fetch(
-    'https://restcountries.com/v3.1/name/Italy?fields=name,capital,population,flags,languages'
-  ).then(response => response.json());
+  return fetch(
+    `${URL}/v3.1/name/${country}?fields=name,capital,population,flags,languages`
+  ).then(response => {
+    if (!response.ok) {
+      throw new Error('fail');
+    }
+    return response.json();
+  });
 }
 
-function createMarkup({ name, capital, population, flags, languages }) {
-  listCountries.innerHTML = `
-        <img src=${flags} alt="flag"/>
-        <h2 class="contry-name">${name}</h2>
-     <ul class="contry-name__list">
-      <li class="contry-name__item">Capital:${capital}</li>
-    <li class="contry-name__item">Population:${population}</li>
-     <li class="contry-name__item">Languages:${languages}</li>
-    </ul>`;
+function createCountryCard(countyInform) {
+  const markup = countryCard(countyInform);
+  listCountries.innerHTML = markup;
 }
+
+// function createMarkup({ name, capital, population, flags, languages }) {
+//   listCountries.innerHTML = `
+//       <img src=${flags} alt="flag"/>
+//       <h2 class="contry-name">${name}</h2>
+//       <ul class="contry-name__list">
+//       <li class="contry-name__item">Capital:${capital}</li>
+//       <li class="contry-name__item">Population:${population}</li>
+//       <li class="contry-name__item">Languages:${languages}</li>
+//       </ul>`;
+// }
